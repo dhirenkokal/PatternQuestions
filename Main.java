663dc1c12099ba9b6f3298245aee13f442a25220
dhirenkokal <dhirenkokal6@gmail.com>
2024-05-31 23:31:58 +0530

    Added RightAngledTrianglePatternNumbers

--- Main.java
+++ Main.java
@@ -28,4 +28,15 @@ public class Main {
         }
     }
 
+    public static void RightAngledTrianglePatternNumbers(int n){
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+   
+
 }
