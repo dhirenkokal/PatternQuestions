1459c3211ec779f1012ef5cb1c219a459aa3294b
dhirenkokal <dhirenkokal6@gmail.com>
2024-06-02 13:33:33 +0530

    Added InvertedRightAngledTrianglePatternStars

--- Main.java
+++ Main.java
@@ -46,4 +46,15 @@ public class Main {
         }
     }
 
+    
+    public static void InvertedRightAngledTrianglePatternStars(int n){
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= n+1-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
